2490809bd497dc95c386228e3bdd5de700595dd0
Sumith Singh <sumithsingh1610@gmail.com>
2026-06-03 16:30:28 +0530

    Setting Up Project (Ecommerce) Fetching-All-Categories ......Adding Categories

---
+++ sb-ecom/src/main/java/Spring/Boot/Ecommerce/Project/SbEcomApplication.java
@@ -0,0 +1,13 @@
+package Spring.Boot.Ecommerce.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbEcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbEcomApplication.class, args);
+	}
+
+}
---
+++ sb-ecom/src/test/java/Spring/Boot/Ecommerce/Project/SbEcomApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Boot.Ecommerce.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbEcomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
